c026d232c01860da2db88d8e14cc35217f8c1879
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-09-09 23:25:25 +0800

    fix: ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -17,7 +17,7 @@ export const ProductDetail = () => {
   return (
     <main>
       <section className="flex flex-col lg:flex-row dark:text-slate-100">
-        <div className="m-3 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="m-3 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div className="flex items-center justify-between py-2 px-2">
             <p className="font-normal text-gray-500 dark:text-gray-100">
               diamond
@@ -35,13 +35,19 @@ export const ProductDetail = () => {
             alt="Nft image"
           />
         </div>
-        <div className="m-3 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt
-            doloremque magnam! Odit, ex voluptatum quidem esse nam ab assumenda,
-            commodi cupiditate repudiandae provident inventore. Veritatis culpa
-            aspernatur nulla adipisci!
-          </p>
+        <div className="m-3 px-5 py-2 w-full">
+          <div className="text-gray-500 dark:text-gray-100 flex items-center justify-between ">
+            <p className="">B1t Cat Gang (check)</p>
+            <div className="flex gap-2">
+              <p>deal</p>
+              <p>share</p>
+              <p>more</p>
+            </div>
+          </div>
+          <h1 className="text-4xl font-bold py-2">B1t Cat Gang #10001</h1>
+          <h2 className="text-xl">
+            Owned by <span className="text-blue-400">DE15246</span>
+          </h2>
         </div>
       </section>
     </main>
