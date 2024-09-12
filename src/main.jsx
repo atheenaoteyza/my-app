3b3d4ce2b3d9ac5ec217e5dedba7d25baa806218
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-09-12 20:19:25 +0800

    build: ScrollToTop component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { ScrollToTop } from "./components/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
+      <ScrollToTop></ScrollToTop>
       <App />
     </Router>
   </StrictMode>
