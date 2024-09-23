b3e11b7a21175ba385ee496a22deb54fad32aa4f
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-09-24 07:33:12 +0800

    feat: Cart Page

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -1,9 +1,10 @@
 import { CartList } from "./components/CartList.jsx";
+import { CartEmpty } from "./components/CartEmpty.jsx";
 export const CartPage = () => {
   return (
     <>
       <main>
-        <CartList></CartList>
+        <CartEmpty />
       </main>
     </>
   );
--- src/pages/Cart/components/CartList.jsx
+++ src/pages/Cart/components/CartList.jsx
@@ -1,10 +1,182 @@
 export const CartList = () => {
+  const cartProduct = true;
   return (
     <>
-      <section>
-        <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">
-          My Cart
-        </p>
+      <section className=" antialiased">
+        <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+            <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
+              <div className="space-y-6">
+                <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                  <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    My Cart
+                  </h2>
+                  {cartProduct && (
+                    <>
+                      <div className="border-t border-gray-200 dark:border-gray-700 mt-5 py-5 space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                        <a href="#" className="shrink-0 md:order-1">
+                          <img
+                            className="h-20 w-20 dark:hidden"
+                            src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg"
+                            alt="imac image"
+                          />
+                          <img
+                            className="hidden h-20 w-20 dark:block"
+                            src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg"
+                            alt="imac image"
+                          />
+                        </a>
+
+                        <div className="flex items-center justify-between md:order-3 md:justify-end">
+                          <div className="text-end md:order-4 md:w-32">
+                            <p className="text-base font-bold text-gray-900 dark:text-white">
+                              $1,499
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                          <a
+                            href="#"
+                            className="text-base font-medium text-gray-900 hover:underline dark:text-white"
+                          >
+                            PC system All in One APPLE iMac (2023) mqrq3ro/a,
+                            Apple M3, 24" Retina 4.5K, 8GB, SSD 256GB, 10-core
+                            GPU, Keyboard layout INT
+                          </a>
+
+                          <div className="flex items-center gap-4">
+                            <button
+                              type="button"
+                              className="inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-900 hover:underline dark:text-gray-400 dark:hover:text-white"
+                            >
+                              <svg
+                                className="me-1.5 h-5 w-5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
+                                />
+                              </svg>
+                              Add to Favorites
+                            </button>
+
+                            <button
+                              type="button"
+                              className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                            >
+                              <svg
+                                className="me-1.5 h-5 w-5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M6 18 17.94 6M18 18 6.06 6"
+                                />
+                              </svg>
+                              Remove
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
+              <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+                <p className="text-xl font-semibold text-gray-900 dark:text-white">
+                  Cart Summary
+                </p>
+
+                <div className="space-y-4">
+                  <div className="space-y-2">
+                    <dl className="flex items-center justify-between gap-4">
+                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Product 1
+                      </dt>
+                      <dd className="text-base font-medium text-gray-900 dark:text-white">
+                        $799
+                      </dd>
+                    </dl>
+                    <dl className="flex items-center justify-between gap-4">
+                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Product 2
+                      </dt>
+                      <dd className="text-base font-medium text-gray-900 dark:text-white">
+                        $799
+                      </dd>
+                    </dl>
+                  </div>
+
+                  <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                    <dt className="text-base font-bold text-gray-900 dark:text-white">
+                      Total
+                    </dt>
+                    <dd className="text-base font-bold text-gray-900 dark:text-white">
+                      $8,191.00
+                    </dd>
+                  </dl>
+                </div>
+
+                <a
+                  href="#"
+                  className="flex w-full items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Proceed to Checkout
+                </a>
+
+                <div className="flex items-center justify-center gap-2">
+                  <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    {" "}
+                    or{" "}
+                  </span>
+                  <a
+                    href="#"
+                    title=""
+                    className="inline-flex items-center gap-2 text-sm font-medium text-blue-700 underline hover:no-underline dark:text-blue-500"
+                  >
+                    Continue Shopping
+                    <svg
+                      className="h-5 w-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 12H5m14 0-4 4m4-4-4-4"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
